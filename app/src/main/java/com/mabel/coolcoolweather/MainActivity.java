9f080a8ca4a7a1cb4e45c2e9686325dcd10915d9
mabelxue <xueym0106@icloud.com>
2017-05-11 09:44:17 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/mabel/coolcoolweather/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mabel.coolcoolweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
